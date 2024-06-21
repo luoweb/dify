69c6a28901719813d191e2191c5c472098fbdcb8
robin <hongxing525@126.com>
2024-06-21 13:06:41 +0800

    fix(ui): custom ui

--- web/custom.sh
+++ web/custom.sh
@@ -16,7 +16,8 @@ $xsed "s#'CAN_REPLACE_LOGO': 'False'#'CAN_REPLACE_LOGO': 'True'#g" ../api/config
 # custom webui
 $xsed "s#Dify#LLM AI#g" ../web/i18n/zh-Hans/login.ts
 $xsed "s#Dify#LLM AI#g" ../web/app/layout.tsx
-$xsed "s#logo-site.png#logo-site-blockai.png#g" ../web/app/components/base/logo/logo-site.tsx
+$xsed "s#POWERED BY#技术支持:#g" ../web/app/components/custom/custom-web-app-brand/index.tsx
+$xsed "s#logo-site.png#logo-site-jsb.png#g" ../web/app/components/base/logo/logo-site.tsx
 $xsed "s#LangGenius, Inc.#BlockAI, Inc.#g" ../web/app/components/header/account-about/index.tsx
 $xsed "s#LangGenius, Inc.#BlockAI, Inc.#g" ../web/app/signin/page.tsx
 $xsed "s#https://dify.ai/#https://llm.ai/#g" ../web/app/components/header/account-about/index.tsx
@@ -25,6 +26,7 @@ $xsed "s#https://dify.ai/#https://llm.ai/#g" ../web/app/components/share/chat/we
 $xsed "s#https://dify.ai/#https://llm.ai/#g" ../web/app/components/share/chatbot/welcome/index.tsx
 $xsed "s#https://dify.ai/#https://llm.ai/#g" ../web/app/signin/normalForm.tsx
 $xsed "s#https://github.com/langgenius/dify/#https://github.com/blockmap/llmai/#g" ../web/app/components/header/account-about/index.tsx
-
+find ../web/ -name "*.tsx" | xargs grep "\- Dify" | awk -F':' '{print $1} | $xsed "s#- Dify#- LLM#g" 
+'
 # custom api:
 # $xsed "s#Dify OpenAPI#BlockAI OpenAPI#g" ../api/controllers/service_api/index.py
